4be2ab3d86ec652cf191abe8b70bbbfab67c5ee3
Sidney Alcantara <sidney@sidney.me>
2019-05-15 12:11:25 +1000

    Basic API with filtering & searching

---
+++ README.md
@@ -0,0 +1,6 @@
+A very basic Express server to provide an API to get shop items.
+
+## To run:
+1. Install using `$ yarn`
+2. `$ yarn start`
+3. Make requests to `http://localhost:2428`
---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const port = 2428;
+
+const data = require('./shop-items.json');
+
+const FuzzySearch = require('fuzzy-search');
+
+// all items & categories
+app.get('/', (req, res) => res.json(data));
+
+// items
+app.get('/items', (req, res) => {
+  let items = data.items;
+
+  if (req.query.category && req.query.category.length > 0)
+    items = data.items.filter(x =>
+      req.query.category.some(y => x.categories.indexOf(y) > -1)
+    );
+
+  if (req.query.search) {
+    const searcher = new FuzzySearch(data.items, ['title', 'id'], {
+      sort: true,
+    });
+
+    items = searcher.search(req.query.search);
+  }
+
+  res.json(items);
+});
+
+// get specific item
+app.get('/item/:itemID', (req, res) => {
+  const filteredItems = data.items.filter(x => x.id === req.params.itemID);
+  if (filteredItems.length > 0) res.json(filteredItems[0]);
+  res.json();
+});
+
+// categories
+app.get('/categories', (req, res) => res.json(data.categories));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
